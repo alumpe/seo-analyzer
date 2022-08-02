d7a8d0d7d7b82584b17b937635a05a0d6babbb02
Adrian Lumpe <git@alumpe.de>
2022-08-02 17:09:53 +0200

    enables enter pressing for url input

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,13 +5,19 @@ import GoogleSnippet from "$lib/snippets/GoogleSnippet.svelte";
 import { fetchMetaData } from "$lib/stores/parsedResult";
 
 let inputValue: string = "https://nodejs.org";
+
+const handleFetch = () => fetchMetaData(inputValue);
 </script>
 
 <div class="layout">
   <div class="table-container">
     <div class="search-bar">
-      <input type="text" bind:value={inputValue} />
-      <button on:click={() => fetchMetaData(inputValue)}>Fetch website</button>
+      <input
+        type="text"
+        bind:value={inputValue}
+        on:keydown={(e) => e.key === "Enter" && handleFetch()}
+      />
+      <button on:click={handleFetch}>Fetch website</button>
     </div>
 
     <PageTable />
